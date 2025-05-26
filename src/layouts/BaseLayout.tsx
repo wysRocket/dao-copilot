9bab58abacf6a96f61d75efca32ced9b947b176e
VladimirK <wysness@gmail.com>
2025-05-26 16:51:45 +0300

    fix: update storage references from S3 to Google Cloud Storage in tasks and reports

--- src/layouts/BaseLayout.tsx
+++ src/layouts/BaseLayout.tsx
@@ -6,7 +6,7 @@ import CustomTitleBar from '@/components/CustomTitleBar';
 export default function BaseLayout({children}: {children: React.ReactNode}) {
   return (
     <>
-      <DragWindowRegion title=" " />
+      <DragWindowRegion title="Drag to move the window" />
       <CustomTitleBar />
       <main className="h-screen p-2 pb-20">{children}</main>
     </>
